3beba918722fb2c4f32ae697a5971a9588cfa5e4
alinzh <siberiangodness@mail.ru>
2023-05-28 20:08:27 +0300

    To fixed url for hosting on google_cloude

--- check_answer.py
+++ check_answer.py
@@ -1,7 +1,7 @@
 import json
 class CheckData:
     def check_city(self, city):
-        with open(r"C:\Users\Пользователь\PycharmProjects\pythonProject_avia\city2code.json", encoding="utf-8") as f:
+        with open(r"city2code.json", encoding="utf-8") as f:
             data2 = json.load(f)
             if city in data2:
                 return True
