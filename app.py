4659f8f74a72e90ce49c1c3d69bdbd73f28dfc42
alinzh <siberiangodness@mail.ru>
2023-05-28 22:53:15 +0300

    Added verification on duration of route and number of citys (users input)

--- app.py
+++ app.py
@@ -2,9 +2,11 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     return render_template('search.html')
+
 
 @app.route('/results', methods=['GET', 'POST'])
 def results():
@@ -15,10 +17,28 @@ def results():
         return_date = request.form['return_date']
         nonstop = request.form.get('nonstop')
         airlines = request.form.getlist('airlines')
-        # perform search with provided parameters
-        # return results to the user
+
+        transit_cities = []
+        transit_times = []
+
+        transit_city_1 = request.form.get('transit_city_1')
+        transit_time_1 = request.form.get('transit_time_1')
+        transit_cities.append(transit_city_1)
+        transit_times.append(transit_time_1)
+
+        transit_city_2 = request.form.get('transit_city_2')
+        transit_time_2 = request.form.get('transit_time_2')
+        transit_cities.append(transit_city_2)
+        transit_times.append(transit_time_2)
+
+        # Продолжайте добавлять transit_city_N и transit_time_N для каждого дополнительного поля
+
+        # Выполните поиск с указанными параметрами
+        # Верните результаты пользователю
         return render_template('results.html')
+
     return 'Invalid request method'
+
 
 if __name__ == '__main__':
     app.run(debug=True)
