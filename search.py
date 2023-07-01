b3d64c5957aee3878c89b992aab8d5665bb9ec45
alinzh <siberiangodness@mail.ru>
2023-07-01 22:35:03 +0300

    Added search in chronological order, changed the interface and the logic of the bot, in connection with this. Added comments to bot.py. Message text removed from file bot.py.

--- search.py
+++ search.py
@@ -12,7 +12,7 @@ import time
 
 class Search():
 
-    def __init__(self, token="TOKEN"):
+    def __init__(self, token="191827beb804bd4d4025b75737717e18"):
         self.token = token
 
     def find_flights_fo_period(self, airports, start_date, end_date, s_period, e_period, home, finish):
